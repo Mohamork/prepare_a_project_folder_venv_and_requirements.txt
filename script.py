1586cd51f1c4de7594f256889e9856ef5bc11f16
Mohamork <k.m19981102@gmail.com>
2026-04-17 22:18:30 +0200

    Refactor script to improve readability and structure

--- script.py
+++ script.py
@@ -8,56 +8,81 @@ import venv
 import subprocess
 
 # Create a name for the project and the venv
+
 project_name = input('Please give a name for this project > ').lower()
+
 venv_name = 'venv'
+
 # Exit and do not create the folder if it exists
 if not os.path.exists(project_name) :
+    
     os.makedirs(project_name)
     print(f'A new folder name: {project_name} is created')
+    
 else: 
+    
     print(project_name,'already exists.','\nExiting.')
     sys.exit()    
+    
 # Get the absolute path for the project and combine with the venv name
+
 project_path = os.path.abspath(project_name)
 venv_path = os.path.join(project_name,venv_name)
+
 # Create the virtual environment
+
 print('Please wait while installing the virtual environment...')
 venv.EnvBuilder(with_pip=True).create(venv_path)
+
 # Use the venv's Python interpreter
+
 if os.name =='nt' :
+    
     python = os.path.join(venv_path,'Scripts','python')   # if on a windows computer
+    
 else :
+    
     python = os.path.join(venv_path,'bin','python') # if on a Linux or Mac
+    
 # Upgrade pip
+
 result = subprocess.check_call([
     python,'-m','pip','install','--upgrade', 'pip'])
+
 print(result)
+
 # Install packages from requirements.txt
+
 try:
+    
     result = subprocess.check_call([
         python, '-m','pip','install','-r', 'requirements.txt'])
+    
 except subprocess.CalledProcessError:
+    
     print()   
+    
 # Create a new requirements.txt inside the projectfolder
+
 with open(f'{project_path}/requirements.txt','w')  as f:
+    
     result = subprocess.check_call([
         python, '-m','pip','freeze'],stdout=f,)
+    
 print('* '*15)
-print(f'requirements.txt is created inside {project_path} for reference for collaborators.')
 
+print(f'requirements.txt is created for project:{project_name')
 
 # Display the installed packages
+
 with open(f'{project_path}/requirements.txt', 'r') as f:
-    pkg = (sum(1 for _ in f)) 
+    
+    pkg = (sum(1 for module in f)) 
+    
 subprocess.check_call([python,'-m','pip','list'])
+
 print(f'Installed {pkg} packages from requirements.txt')
 
-'''
-
-with open(f'{project_path}/requirements.txt', 'r') as f:
-    print(sum(1 for pkg in f)) 
-
-'''
 
 
 
